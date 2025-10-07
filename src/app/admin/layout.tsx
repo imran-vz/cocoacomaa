43e56543d47ae00f83fcfff3ffbc7ddb56c65bc8
Mohammed Imran <mohammedimran86992@gmail.com>
2025-10-07 12:24:39 +0530

    Update admin layout to improve overflow handling
    - Adjusted the layout in `AdminDashboardLayout` to set a maximum height and enable vertical scrolling for better content management.
    - This change enhances the user experience by preventing overflow issues and ensuring that all content remains accessible.

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -41,7 +41,7 @@ export default async function AdminDashboardLayout({
 			<AppSidebar variant="inset" />
 			<SidebarInset>
 				<SiteHeader />
-				<div className="flex flex-1 flex-col">
+				<div className="flex flex-1 max-h-[calc(100svh-2rem)] overflow-y-auto flex-col">
 					<div className="@container/main flex flex-1 flex-col gap-2">
 						<div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
 							{children}
