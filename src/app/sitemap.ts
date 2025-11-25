8e554396a3f36621eb42aa20fbf70afe031f73d4
Mohammed Imran <mohammedimran86992@gmail.com>
2025-11-25 13:03:54 +0530

    Updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -47,13 +47,25 @@ export default function sitemap(): MetadataRoute.Sitemap {
 			priority: 0.7,
 		},
 		{
-			url: `${baseUrl}/privacy-policy`,
+			url: `${baseUrl}/data-protection`,
 			lastModified: new Date(),
 			changeFrequency: "yearly",
 			priority: 0.3,
 		},
 		{
-			url: `${baseUrl}/terms`,
+			url: `${baseUrl}/terms-of-use`,
+			lastModified: new Date(),
+			changeFrequency: "yearly",
+			priority: 0.3,
+		},
+		{
+			url: `${baseUrl}/terms-of-sale`,
+			lastModified: new Date(),
+			changeFrequency: "yearly",
+			priority: 0.3,
+		},
+		{
+			url: `${baseUrl}/terms-and-conditions`,
 			lastModified: new Date(),
 			changeFrequency: "yearly",
 			priority: 0.3,
